3a600f0f5f52898ff6dc9bc14b2a2196b42658b1
Joery Vreijsen <joery_vreijsen@hotmail.com>
2025-12-01 17:18:05 +0100

    Solution day 1

--- src/main/kotlin/be/vreijsenj/aoc/days/Day01.kt
+++ src/main/kotlin/be/vreijsenj/aoc/days/Day01.kt
@@ -1,9 +1,44 @@
 package be.vreijsenj.aoc.days
 
 import be.vreijsenj.aoc.utils.PuzzleUtils
+import kotlin.math.abs
 import kotlin.time.measureTime
 
 object Day01 {
+
+    data class DialRotation(val amount: Int) {
+        companion object {
+            @JvmStatic
+            fun parse(line: String): DialRotation {
+                val amount = line.substring(1).toInt()
+
+                return DialRotation(
+                    if("L" in line) -amount else amount
+                )
+            }
+        }
+
+        fun execute(start: Int, max: Int): Int {
+            return (start + amount).mod(max)
+        }
+
+        fun clicks(start: Int, max: Int): Pair<Int, Int> {
+            val loops: Int = abs(amount / max)
+            val rest: Int = amount % max
+
+            val result = execute(start, max)
+
+            if(rest == 0 || start == 0) {
+                return Pair(result, loops)
+            }
+
+            if((start + rest) >= max || (start + rest) <= 0)  {
+                return Pair(result, loops + 1)
+            }
+
+            return Pair(result, loops)
+        }
+    }
 
     @JvmStatic
     fun main(args: Array<String>) {
@@ -21,10 +56,32 @@ object Day01 {
     }
 
     fun runPartOne(input: List<String>): Int {
-        return 0
+        val rotations: List<DialRotation> = input
+            .map { DialRotation.parse(it) }
+
+        val starts: MutableList<Int> = mutableListOf(50)
+
+        rotations.forEach {
+            starts.add(
+                it.execute(starts.last(), 100)
+            )
+        }
+
+        return starts.count { it == 0 }
     }
 
     fun runPartTwo(input: List<String>): Int {
-        return 0
+        val rotations: List<DialRotation> = input
+            .map { DialRotation.parse(it) }
+
+        val starts: MutableList<Int> = mutableListOf(50)
+
+        return rotations.sumOf {
+            val (end, clicks) = it.clicks(starts.last(), 100)
+
+            starts.add(end)
+
+            clicks
+        }
     }
 }
--- src/test/groovy/be/vreijsenj/aoc/days/Day01Test.groovy
+++ src/test/groovy/be/vreijsenj/aoc/days/Day01Test.groovy
@@ -4,26 +4,48 @@ import spock.lang.Specification
 
 class Day01Test extends Specification {
 
-    def "returns total ..."() {
+    def "returns total number of times the dial is at 0"() {
         given: "the example puzzle input"
-        def input = [ ]
+        def input = [
+            "L68",
+            "L30",
+            "R48",
+            "L5",
+            "R60",
+            "L55",
+            "L1",
+            "L99",
+            "R14",
+            "L82"
+        ]
 
-        when: "the ... is calculated"
+        when: "the total number is calculated"
         def result = new Day01().runPartOne(input)
 
         then: "the result matches the example answer"
-        result == 1
+        result == 3
     }
 
-    def "returns total ..."() {
+    def "returns total number of clicks on number 0"() {
         given: "the example puzzle input"
-        def input = [ ]
+        def input = [
+                "L68",
+                "L30",
+                "R48",
+                "L5",
+                "R60",
+                "L55",
+                "L1",
+                "L99",
+                "R14",
+                "L82"
+        ]
 
-        when: "the ... is calculated"
+        when: "the total clicks is calculated"
         def result = new Day01().runPartTwo(input)
 
         then: "the result matches the example answer"
-        result == 1
+        result == 6
     }
 
 }
